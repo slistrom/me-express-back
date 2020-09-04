515285dabc1f05bf679b4ef003dac7e3efc068d0
Stefan <stefan@listrom.se>
2020-09-04 12:05:43 +0200

    Added report handling to API

--- routes/reports.js
+++ routes/reports.js
@@ -1,14 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/texts.sqlite');
+const jwt = require('jsonwebtoken');
 
-router.get('/', function(req, res, next) {
-    const data = {
-        data: {
-            msg:  "Report page"
+let config;
+
+try {
+    config = require('../config.js');
+} catch (error) {
+    console.error(error);
+}
+
+const jwtSecret = process.env.JWT_SECRET || config.secret;
+
+router.post("/",
+    (req, res, next) => checkToken(req, res, next),
+    (req, res) => reportedit(res, req.body));
+
+function checkToken(req, res, next) {
+    const token = req.headers['x-access-token'];
+
+    jwt.verify(token, jwtSecret, function(err, decoded) {
+        if (err) {
+            // send error response
         }
-    };
+        // Valid token send on the request
+        next();
+    });
+}
 
-    res.json(data);
-});
+function reportedit(res, body) {
+    const week = body.week;
+    const text = body.text;
+
+    db.run("UPDATE reports SET text = '" + text + "' WHERE week = " + week,
+        (err) => {
+            if (err) {
+                // console.log(err);
+            }
+            return res.status(201).json({
+                data: {
+                    message: "Report successfully updated."
+                }
+            });
+        });
+}
 
 module.exports = router;
